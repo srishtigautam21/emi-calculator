f44a1f76370b96a8134a4e09e0e1c181d792a2cc
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-02-12 16:03:40 +0530

    Intial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className='App'>Hello</div>;
+  const [cost, setCost] = useState(0);
+  return (
+    <div className='App'>
+      <div className='header'>EMI Calculator</div>
+      <label>
+        Total Cost of Asset
+        <input
+          type='number'
+          value={cost}
+          placeholder='Enter your asset value'
+        ></input>
+      </label>
+      <label>
+        Interest Rate (in %)
+        <input
+          type='number'
+          value={cost}
+          placeholder='Enter interest rate'
+        ></input>
+      </label>
+      <label>
+        Processing Fee (in %)
+        <input type='number' value={cost} placeholder='Processing fee'></input>
+      </label>
+    </div>
+  );
 }
 
 export default App;

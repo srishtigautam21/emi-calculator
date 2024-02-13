47c1641173da4009f39a3b614a69772233795e1b
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-02-13 17:14:06 +0530

    Added down payment input

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
 import "./App.css";
 import React, { useState } from "react";
+import DownPayment from "./component/DownPayment.tsx";
 
 function App() {
   const [cost, setCost] = useState(0);
   return (
-    <div className='App'>
+    <div className='wrapper'>
       <div className='header'>EMI Calculator</div>
-      <label>
-        Total Cost of Asset
+      <label className='inputWrapper'>
+        <p className='title'>Total Cost of Asset</p>
         <input
           type='number'
           value={cost}
           placeholder='Enter your asset value'
+          className='input'
         ></input>
       </label>
-      <label>
-        Interest Rate (in %)
+      <label className='inputWrapper'>
+        <p className='title'>Interest Rate (in %)</p>
         <input
           type='number'
           value={cost}
           placeholder='Enter interest rate'
+          className='input'
         ></input>
       </label>
-      <label>
-        Processing Fee (in %)
-        <input type='number' value={cost} placeholder='Processing fee'></input>
+      <label className='inputWrapper'>
+        <p className='title'>Processing Fee (in %)</p>
+        <input
+          type='number'
+          value={cost}
+          placeholder='Processing fee'
+          className='input'
+        ></input>
       </label>
+      <DownPayment />
     </div>
   );
 }
